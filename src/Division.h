3eba66c73d63d6215a56a5377b4ad8933bffe15d
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2019-09-28 17:20:09 -0400

    No, not division flags.

--- src/Division.h
+++ src/Division.h
@@ -6,16 +6,10 @@
 /** Define the divisions of output; these go in separate sections, and
  approximately follow namespaces. */
 #define DIVISION(X) \
-	X(DIV0, "do not use"), \
 	X(DIV_PREAMBLE, "preamble"), \
 	X(DIV_FUNCTION, "fn"), \
-	X(DIV3, "do not use"), \
 	X(DIV_TAG,      "tag"), \
-	X(DIV5, "do not use"), X(DIV6, "do not use"), X(DIV7, "do not use"), \
 	X(DIV_TYPEDEF,  "typedef"), \
-	X(DIV9,  "do not use"), X(DIV10, "do not use"), X(DIV11, "do not use"), \
-	X(DIV12, "do not use"), X(DIV13, "do not use"), X(DIV14, "do not use"), \
-	X(DIV15, "do not use"), \
 	X(DIV_DATA,     "data")
 
 enum Division { DIVISION(PARAM2A) };
