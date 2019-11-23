822c310fb166be561dcf6f5c2121655e83281d1d
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2019-11-23 09:24:07 -0500

    Test all in one.

---
+++ samples/foo.h
@@ -0,0 +1,10 @@
+/** In the header.
+ @param[ALLYOURBASE] Also in the header.
+ @fixme
+ @author */
+struct Cee;
+
+/** Prototype! */
+struct Cee *Cee();
+void Cee_(struct Cee **ceeptr);
+char *CeeGetVar(const struct Cee *cee);
