fc532efe427b17aff9acb91a74afe83d1e774cec
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2019-12-13 18:50:09 -0500

    Test.

--- src/Report.c
+++ src/Report.c
@@ -162,7 +162,8 @@ static void erase_segment(struct Segment *const segment) {
 	segment->division = DIV_PREAMBLE;
 	TokenArray_(&segment->doc);
 	TokenArray_(&segment->code);
-	fprintf(stderr, "*** Clearing %s %s index array.\n",
+	if(IndexArraySize(&segment->code_params))
+		fprintf(stderr, "*** Clearing %s: %s.\n",
 		a, IndexArrayToString(&segment->code_params));
 	IndexArray_(&segment->code_params);
 	attributes_(&segment->attributes);
@@ -293,8 +294,8 @@ static int report_semantic(struct Segment *const segment) {
 	{
 		char a[12];
 		segment_to_string(segment, &a);
-		fprintf(stderr, "*** Segment %s added %lu to index array %s.\n",
-			a, (unsigned long)no, IndexArrayToString(&segment->code_params));
+		fprintf(stderr, "*** Adding %lu to %s: %s.\n",
+			(unsigned long)no, a, IndexArrayToString(&segment->code_params));
 	}
 	return 1;
 }
